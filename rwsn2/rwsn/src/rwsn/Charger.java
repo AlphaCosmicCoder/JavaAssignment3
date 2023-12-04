c9a1387d368bfe8d1e2ca794d7262b85c1cb5225
Ayush Singh Rajput <83543768+AlphaCosmicCoder@users.noreply.github.com>
2023-12-04 10:00:38 +0530

    new

---
+++ rwsn2/rwsn/src/rwsn/Charger.java
@@ -0,0 +1,60 @@
+package rwsn;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.List;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+import javax.swing.ImageIcon;
+
+public class Charger implements DisplayObject {
+	Image img;
+	int x, y, id;
+	double remainingEnergy;
+	public Charger(int x, int y, int id) {
+		this.x = x;
+		this.y = y;
+		this.id = id;
+		remainingEnergy = Parameters.InitialEnergy;
+		img = new ImageIcon(getClass().getResource("../images/charger.png")).getImage();
+	}
+
+	Queue<Sensor> toBeChargedSensors = new ArrayDeque<Sensor>();
+
+	// Add a sensor to this charger's queue
+	public void addSensor(Sensor S) {
+		toBeChargedSensors.add(S);
+		System.out.println("SENSOR " + S.id + " ASSIGNED TO CHARGER " + this.id);
+	}
+
+	// Charge a sensor from the queue
+	public void chargeSensor() {
+		if(!toBeChargedSensors.isEmpty()) {
+			// Get the first sensor in queue
+			Sensor S = toBeChargedSensors.poll();
+			// Calculate the energy needed by the sensor
+			double chargeNeeded = Parameters.InitialEnergy - S.remainingEnergy;
+
+			// If the charger doesn't have enough energy, deplete the charger
+			if(chargeNeeded >= this.remainingEnergy) {
+				S.remainingEnergy += this.remainingEnergy;
+				this.remainingEnergy = 0;
+			}
+			// else provide required energy to the sensor
+			else {
+				S.remainingEnergy += chargeNeeded;
+				this.remainingEnergy -= chargeNeeded;
+			}
+		}
+	}
+	
+	@Override
+	public void draw(Graphics2D g) {
+		g.drawImage(img,x,y,50,70,null);
+		g.drawString(String.valueOf(remainingEnergy), x, y+5);
+		g.drawString(String.valueOf(this.id), x, y+40);
+	}
+
+}
---
+++ rwsn2/rwsn/src/rwsn/MainFrame.java
@@ -0,0 +1,122 @@
+package rwsn;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class MainFrame extends JFrame implements ActionListener{
+
+	DisplayCanvas canvas = new DisplayCanvas();
+	static ArrayList<Sensor> sensorsList = new ArrayList<Sensor>();
+	static ArrayList<Charger> chargersList = new ArrayList<Charger>();
+	
+
+	public MainFrame() {
+		setTitle("Rechargable WSN");
+		setExtendedState(MAXIMIZED_BOTH);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		add(canvas);
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		BaseStation bs = new BaseStation((int)((d.getWidth()-20)/2),(int)(d.getHeight()/2));
+		canvas.bs = bs;
+		int ns = Integer.parseInt(JOptionPane.showInputDialog("How Many Sensors?"));
+		int nc = Integer.parseInt(JOptionPane.showInputDialog("How Many Chargers?"));
+		
+		for(int i=0;i<nc;i++) {
+			int x = (int)(Math.random()*d.getWidth()+0.5);
+			int y = (int)(Math.random()*(d.getHeight()-150)+0.5);					
+			Charger c = new Charger(x, y, i);
+			canvas.chargers.add(c);
+			chargersList.add(c);
+		}
+		for(int i=0;i<ns;i++) {
+			int x = (int)(Math.random()*d.getWidth()+0.5);
+			int y = (int)(Math.random()*(d.getHeight()-150)+0.5);					
+			Sensor s = new Sensor(x, y, i, bs);
+			canvas.sensors.add(s);
+			sensorsList.add(s);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		MainFrame f = new MainFrame();
+		f.setVisible(true);
+		
+		PriorityQueue<Integer> lowEnergySensorList = new PriorityQueue<Integer>();
+
+		while(true) {
+			// get data from sensors
+			for(Sensor o:sensorsList) {
+				Message M = o.getData();
+				// Send message data to baseStation
+				f.canvas.bs.addMessage(M);
+
+				// Check for sensor energy
+				if(o.needCharge) {
+					if(!lowEnergySensorList.contains(o.id)) {
+						lowEnergySensorList.add(o.id);
+					}
+				}
+			}
+
+			// Display the data received from the sensors
+			f.canvas.bs.DisplayMessages();
+			
+
+			// ---CHARGING---
+			if(!lowEnergySensorList.isEmpty()) {
+				// Get sensor that requires charging with highest priority
+				int chargeRequestId = lowEnergySensorList.poll();
+				// Find nearest charger for the sensor
+				Sensor needsChargeS = sensorsList.get(chargeRequestId);
+				double distance = 9999.0f;
+				int chargerID = -1;
+				for(Charger o:chargersList) {
+					// Check if the charger has enough energy to charge the sensor
+					if(o.remainingEnergy <= 0) {
+						// if charger has been depleted then look for the next nearest charger with energy
+						continue;
+					}
+
+					// Calculate distance from charger to sensor to find the nearest charger
+					double tempD;
+					tempD = Math.sqrt((Math.pow((needsChargeS.x - o.x), 2)) + (Math.pow((needsChargeS.y - o.y), 2)));
+					if(tempD <= distance) {
+						distance = tempD;
+						chargerID = o.id;
+					}
+				}
+				// Add the sensor to the charger's queue
+				if(chargerID != -1)
+					chargersList.get(chargerID).addSensor(needsChargeS);
+			}
+
+			// Charge sensors in each Charger's queue
+			for(Charger o:chargersList) {
+				o.chargeSensor();
+			}
+
+			f.canvas.repaint();
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
